80b278e62af86b401024ff59fef9a41ef8ffb2f6
Laurent Meirlaen <burner@live.be>
2014-12-14 00:14:38 +0100

    Change scoreboard columns order

--- DemoParser-Model/Models/ScoreBoardLine.cs
+++ DemoParser-Model/Models/ScoreBoardLine.cs
@@ -10,11 +10,11 @@ namespace DemoParser_Model.Models
 	{
 		public string PlayerName { get; set; }
 		public int Kills { get; set; }
-		public int Headshot { get; set; }
 		public int Assist { get; set; }
 		public int Deaths { get; set; }
 		//public int Score { get { return (2 * Kills + Assist); } } TODO compute CSGO Score (bomb plant/defuse)
 		public int Mvp { get; set; }
+		public int Headshot { get; set; }
 		public double HSR { get { return Math.Round((double)Headshot / Kills, 2); } }
 		public double KDR { get { return Math.Round((double)Kills / Deaths, 2); } }
 		public int KDdiff { get { return Kills - Deaths; } }
