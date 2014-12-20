c00a59a05a928649b8acf8b8c1747ccfaa0bf32c
Laurent Meirlaen <burner@live.be>
2014-12-20 13:28:50 +0100

    Create new console project for test only

---
+++ DemoParser-Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using DemoParser_Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoParser_Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(DateTime.Now);
+			Stream inputStream = new FileStream(args[0], FileMode.Open);
+			DemoParser parser = new DemoParser(inputStream);
+			parser.ParseDemo(true);
+			Console.WriteLine(DateTime.Now);
+			Console.ReadKey();
+		}
+	}
+}
